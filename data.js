97bfe95402cec056d0f9c48e105fae04247588d4
Anuragtech02 <apal895@gmail.com>
2020-06-13 13:00:08 +0530

    Initial commit

---
+++ data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-bryan-goff-iuyhxaia8ea-unsplash",
+      "name": "bryan-goff-IuyhXAia8EA-unsplash",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": 2.6102658566887262,
+        "pitch": 0.06162265909901876,
+        "fov": 1.362234561578609
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
