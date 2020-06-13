5127d29ca451638b3cfc7797023695ce4d41d83b
Anuragtech02 <apal895@gmail.com>
2020-06-13 13:15:52 +0530

    Updated files

--- data.js
+++ data.js
@@ -1,7 +1,45 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-bryan-goff-iuyhxaia8ea-unsplash",
+      "id": "0-timothy-oldfield-luufnhochru-unsplash",
+      "name": "timothy-oldfield-luufnHoChRU-unsplash",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.44858808958650087,
+        "pitch": 0.08321137542200141,
+        "fov": 1.362234561578609
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.47134723880694374,
+          "pitch": -0.3456157713637058,
+          "rotation": 0,
+          "target": "1-bryan-goff-iuyhxaia8ea-unsplash"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-bryan-goff-iuyhxaia8ea-unsplash",
       "name": "bryan-goff-IuyhXAia8EA-unsplash",
       "levels": [
         {
@@ -24,9 +62,9 @@ var APP_DATA = {
       ],
       "faceSize": 1500,
       "initialViewParameters": {
-        "yaw": 2.6102658566887262,
-        "pitch": 0.06162265909901876,
-        "fov": 1.362234561578609
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
       "linkHotspots": [],
       "infoHotspots": []
@@ -35,8 +73,8 @@ var APP_DATA = {
   "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
-    "fullscreenButton": false,
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
     "viewControlButtons": false
   }
 };
