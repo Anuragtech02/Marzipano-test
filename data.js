61252644466b2ad89853ef4b166b3855e823c6e1
Anuragtech02 <apal895@gmail.com>
2020-06-13 13:29:18 +0530

    New Update

--- data.js
+++ data.js
@@ -34,12 +34,18 @@ var APP_DATA = {
           "pitch": -0.3456157713637058,
           "rotation": 0,
           "target": "1-bryan-goff-iuyhxaia8ea-unsplash"
+        },
+        {
+          "yaw": -0.47976531265060984,
+          "pitch": 0.33948764910126883,
+          "rotation": 0,
+          "target": "2-1-1"
         }
       ],
       "infoHotspots": [
         {
-          "yaw": -0.28282141978176867,
-          "pitch": -0.3488148113704739,
+          "yaw": -0.28282140275361556,
+          "pitch": -0.34881481151407456,
           "title": "Click me",
           "text": "You can click the arrow button to move to another page"
         }
@@ -68,6 +74,29 @@ var APP_DATA = {
         }
       ],
       "faceSize": 1500,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-1-1",
+      "name": "1 (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 450,
       "initialViewParameters": {
         "pitch": 0,
         "yaw": 0,
